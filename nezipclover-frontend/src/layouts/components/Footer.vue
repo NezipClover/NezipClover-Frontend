4204cdbea4b23bbf6bf7f054cd8b6bb550eae994
xfrnk2 <xfrnk2@gmail.com>
2022-11-24 17:38:25 +0900

    refactor: delete footer

--- nezipclover-frontend/src/layouts/components/Footer.vue
+++ nezipclover-frontend/src/layouts/components/Footer.vue
@@ -19,19 +19,6 @@
       >NezipClover</a>
     </span>
     <!-- 👉 Footer: right content -->
-    <span class="d-md-flex gap-x-4 text-primary d-none">
-      <a
-        href="https://themeselection.com/license/"
-        target="noopener noreferrer"
-      >License</a>
-      <a
-        href="https://themeselection.com/"
-        target="noopener noreferrer"
-      >More Themes</a>
-      <a
-        href="https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/documentation/"
-        target="noopener noreferrer"
-      >Documentation</a>
-    </span>
+
   </div>
 </template>
